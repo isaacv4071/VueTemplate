baeb828af974432b412aed7e652179abcdb355f0
Geovani <geovanibracamonte4@gmail.com>
2024-05-04 13:57:55 -0500

    Se cargan los select y se modifican estilos

--- src/components/Tables/TablaList.vue
+++ src/components/Tables/TablaList.vue
@@ -4,16 +4,34 @@ import axios from 'axios'
 import { ref } from 'vue'
 import { apiUrl } from '@/config/apiConfig.js' //Importa la URL de la API
 
+// Refs para almacenar los datosammm
 const listado = ref([])
+const periodos = ref([])
+const facultades = ref([])
+const programas = ref([])
 
-onMounted(() => {
+//Realizar la solicitud HTTP y manejar la respuesta
+const fetchDta = () => {
   axios
     .get(apiUrl)
     .then(function (response) {
       // manejar respuesta exitosa
 
+      //Se almacennan datos de la  API
       listado.value = response.data
-      //console.log(response.data)
+
+      //Se obtienen períodos unicos
+      const periodosUnicos = [...new Set(response.data.map((item) => item.periodo))]
+      periodos.value = periodosUnicos
+      //console.log('Periodos unicos:', periodosUnicos);
+
+      //Se obtienen facultades únicas
+      const facultadesUnicas = [...new Set(response.data.map((item) => item.facultad))]
+      facultades.value = facultadesUnicas
+
+      //Se obtienen programas únicos
+      const programasUnicos = [...new Set(response.data.map((item) => item.programa))]
+      programas.value = programasUnicos
     })
     .catch(function (error) {
       // manejar error
@@ -22,7 +40,10 @@ onMounted(() => {
     .finally(function () {
       // siempre sera executado
     })
-})
+}
+
+//Se ejecuta el fetchData cuando se monta el componente
+onMounted(fetchDta)
 </script>
 
 <template>
@@ -31,70 +52,67 @@ onMounted(() => {
   >
     <h4 class="mb-2 text-xl font-semibold text-black dark:text-white">Filtro</h4>
 
-    <div class="flex mb-4 space-x-3">
+    <div class="flex mb-4 space-x-3 dark:bg-form-input dark:border-strokedark p-5 border border-stroke rounded-sm">
+      <!-- Select para elegir período -->
       <div class="flex-1">
         <form class="max-w-sm mx-auto">
-          <label
-            for="countries"
-            class="mb-3 block text-sm font-medium text-black dark:text-white"
+          <label for="periodo" class="mb-3 block text-sm font-medium text-black dark:text-white"
             >Seleccione período</label
           >
           <select
-            id="countries"
-            class=" dark:bg-form-input bg-gray-50 border border-stroke text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            id="periodo"
+            class="dark:bg-form-input bg-gray-50 border border-stroke text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option disabled selected>Período</option>
-            <option value="US" >United States</option>
-            <option value="CA">Canada</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
+            <!-- Se itera sobre los periodos únicos y se muestran -->
+            <option v-for="periodo in periodos" value="periodo">{{ periodo }}</option>
           </select>
         </form>
       </div>
+      <!-- Select para elegir facultad -->
       <div class="flex-1">
         <form class="max-w-sm mx-auto">
-          <label
-            for="countries"
-            class="mb-3 block text-sm font-medium text-black dark:text-white"
+          <label for="facultad" class="mb-3 block text-sm font-medium text-black dark:text-white"
             >Selecione Facultad</label
           >
           <select
-            id="countries"
-            class=" dark:bg-form-input bg-gray-50 border border-stroke text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            id="facultad"
+            class="dark:bg-form-input bg-gray-50 border border-stroke text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option disabled selected>Facultad</option>
-            <option value="US" >United States</option>
-            <option value="CA">Canada</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
+            <!-- Se itera sobre las facultades únicas y se muestran -->
+            <option v-for="facultad in facultades" value="facultad">{{ facultad }}</option>
           </select>
         </form>
       </div>
+      <!-- Select para elegir programa -->
       <div class="flex-1">
         <form class="max-w-sm mx-auto">
-          <label
-            for="countries"
-            class="mb-3 block text-sm font-medium text-black dark:text-white"
+          <label for="programa" class="mb-3 block text-sm font-medium text-black dark:text-white"
             >Seleccione Programa</label
           >
           <select
-            id="countries"
-            class=" dark:bg-form-input bg-gray-50 border border-stroke text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            id="programa"
+            class="dark:bg-form-input bg-gray-50 border border-stroke text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option disabled selected>Programa</option>
-            <option value="US" >United States</option>
-            <option value="CA">Canada</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
+            <!-- Se itera sobre los programas únicos y se muestran -->
+            <option v-for="programa in programas" value="programa">{{ programa }}</option>
           </select>
         </form>
       </div>
-      <div>
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-80 mt-8">Buscar</button>
+      <div class="flex items-center">
+
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-80 mt-8"
+        >
+          Buscar
+        </button>
       </div>
     </div>
-    
 
+    <!-- Tabla para mostrar resultados -->
     <div class="relative overflow-x-auto">
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead
