770c5566ce33d7c2ed88427704ffd4d3d2697204
Geovani <geovanibracamonte4@gmail.com>
2024-05-05 23:16:27 -0500

    Boton arreglado

--- src/components/Filters/FilterTable.vue
+++ src/components/Filters/FilterTable.vue
@@ -48,7 +48,7 @@
         <div class="flex items-center">
             <button type="button"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-80 mt-8"
-                @click="filtrado">
+                @click="filtrado(); $event.target.blur()">
                 Buscar
             </button>
         </div>
